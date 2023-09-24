76f66baf03b2d1cdaee9352a144dbfdbca938ff6
uzzal71 <uzzalroy.acm@gmail.com>
2023-09-24 19:13:05 +0600

    creating and using array

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
-import (
-	"io"
-	"net/http"
-	"os"
-)
+import "fmt"
 
 func main() {
-	http.HandleFunc("/", Handler);
-	http.ListenAndServe(":3000", nil)
-}
+	var arr [3]string
+	fmt.Println(arr)
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	f, _ := os.Open("./menu.txt");
-	io.Copy(w, f)
+	arr = [3]string{"Coffee", "Espresso", "Cappuccino"}
+	fmt.Println(arr)
+
+	fmt.Println(arr[1])
+	arr[1] = "Chai Tea"
+
+	fmt.Println(arr)
+
+	arr2 := arr
+
+	arr2[2] = "Chai Latte"
+
+	fmt.Println(arr, arr2)
 }
