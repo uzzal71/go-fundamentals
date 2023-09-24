b0d796bb39408aead5c167d340b8d7fef27595a2
uzzal71 <uzzalroy.acm@gmail.com>
2023-09-24 19:33:06 +0600

    start maps

--- main.go
+++ main.go
@@ -1,27 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"golang.org/x/exp/slices"
-)
-
 func main() {
-	var s []string
-	fmt.Println(s)
-
-	s = []string{"Coffee", "Espresso", "Cappuccino"}
-	fmt.Println(s)
-
-	fmt.Println(s[1])
-	s[1] = "Chai Tea"
-	fmt.Println(s)
-	s = append(s, "Hot Chocolate", "Hot Tea")
-	fmt.Println(s)
-
-	slices.Delete(s, 1, 2)
-
-	s2 := s
-	s2[2] = "Chai Latte"
-	fmt.Println(s, s2)
+	
 }
