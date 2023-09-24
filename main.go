4b44c79520668c9a7fc787ed6db0da1563b54094
uzzal71 <uzzalroy.acm@gmail.com>
2023-09-24 23:13:20 +0600

    maps

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	var m map[string]int
+	fmt.Println(m)
+
+	m = map[string]int{"foo": 1, "bar": 2}
+	fmt.Println(m)
+
+	fmt.Println(m["foo"])
+	m["bar"] = 88
+
+	delete(m, "foo")
+	m["baz"] = 784
+	fmt.Println(m)
+
+	fmt.Println(m["foo"])
+	v, ok := m["foo"]
+	fmt.Println(v, ok);
 }
