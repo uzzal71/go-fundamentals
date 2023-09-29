34c8f02831520b68a4bff27f50097c7b220f95f0
uzzal71 <uzzalroy.acm@gmail.com>
2023-09-29 22:28:49 +0600

    Loop discause

--- main.go
+++ main.go
@@ -1,33 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	// for { ... } infinite loop
-	// for condition { ... } loop till condition
-	// for initializer; test; post clause { ... } counter-base loop
-	// Infinite Loops
-	/*
-	i := 1
-	for {
-		fmt.Println(i)
-		i += 1
-	}
-	*/
-
-	// Loop till condition
-	/*
-	i := 1
-	for i < 3 {
-		fmt.Println(i)
-		i += 1
-	}
-	fmt.Println("Done!")
-	*/
-
-	// Counter-based Loops
-	for i:=1; i < 3; i++ {
-		fmt.Println(i)
-	}
-	fmt.Println("Done!")
+	
 }
