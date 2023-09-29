73b7acb8b1e3fa8989cc1bec758044aa6d00caa1
uzzal71 <uzzalroy.acm@gmail.com>
2023-09-29 22:19:14 +0600

    Infinite Loops

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 	// for { ... } infinite loop
 	// for condition { ... } loop till condition
 	// for initializer; test; post clause { ... } counter-base loop
+	// Infinite Loops
+	i := 1
+	for {
+		fmt.Println(i)
+		i += 1
+	}
 }
