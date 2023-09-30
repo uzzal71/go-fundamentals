01695bc9a8145f298d21eb675fede18c149a26e8
uzzal71 <uzzalroy.acm@gmail.com>
2023-09-30 23:57:10 +0600

    switch

--- main.go
+++ main.go
@@ -1,15 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	// i := 15
-	if i := 15; i < 5 {
-		fmt.Println("i is less than 5")
-	} else if i < 10 {
-		fmt.Println("i less than 10")
-	} else {
-		fmt.Println("i is at least 10")
-	}
-	fmt.Println("after the if statement")
+	
 }
