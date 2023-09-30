6594789e6ce20e585779765591253433fd91e153
uzzal71 <uzzalroy.acm@gmail.com>
2023-10-01 00:33:38 +0600

    panic start

--- main.go
+++ main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"database/sql"
-	"fmt"
-)
-
 func main() {
-	fmt.Println("main 1")
-	defer fmt.Println("defer 1")
-	fmt.Println("main 2")
-	defer fmt.Println("defer 2")
-
-	// defer use database connection
-	db, _ := sql.Open("driverName", "connection string")
-	defer db.Close()
-
-	rows, _ := db.Query("some sql query here")
-	defer rows.Close()
+	
 }
