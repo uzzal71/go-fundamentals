59debebf8e12a0156dc0560c1901f0e3bee6628e
uzzal71 <uzzalroy.acm@gmail.com>
2023-10-01 00:55:21 +0600

    end of session

--- main.go
+++ main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	dividend, divisor := 10, 5
-	fmt.Printf("%v divided by %v is %v\n", dividend, divisor, divide(dividend, divisor))
-
-	dividend, divisor = 10, 0
-	fmt.Printf("%v divided by %v is %v\n", dividend, divisor, divide(dividend, divisor))
-}
-
-func divide(dividend, divisor int) int {
-	defer func() {
-		if msg := recover(); msg != nil {
-			fmt.Println(msg)
-		}
-	}()
-	return dividend / divisor;
+	
 }
