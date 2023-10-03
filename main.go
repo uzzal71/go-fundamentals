8e14d67a366921243e14c21cf8b10a78e9cd80d7
uzzal71 <uzzalroy.acm@gmail.com>
2023-10-04 00:01:38 +0600

    methods

--- main.go
+++ main.go
@@ -3,3 +3,19 @@ package main
 func main() {
 	
 }
+
+// function
+var i int 
+
+func isEvent(i int) bool {
+	return  i% 2 == 0
+}
+ans := isEven(i)
+
+// method
+type myInt int
+var mi myInt
+func (i myInt) isEven() bool {
+	return int(i) % 2 == 0
+}
+ans = mi.isEven()
