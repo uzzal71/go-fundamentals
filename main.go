b56a2de9f6b146f2dbde89e75af75e977624eafb
uzzal71 <uzzalroy.acm@gmail.com>
2023-10-03 19:17:15 +0600

    package

--- main.go
+++ main.go
@@ -7,15 +7,6 @@ import (
 	"strings"
 )
 
-type menuItem struct {
-	name   string
-	prices map[string]float64
-}
-
-var menu = []menuItem{
-	{name: "Coffee", prices: map[string]float64{"small": 1.54, "medium": 1.74, "large": 1.98}},
-	{name: "Espresso", prices: map[string]float64{"small": 1.25, "medium": 2.74, "triple": 1.51}},
-}
 
 var in = bufio.NewReader(os.Stdin)
 
@@ -37,23 +28,6 @@ loop:
 			break loop
 		default:
 			fmt.Println("Unknown option")
-		}
-	}
-}
-
-func addItem() {
-	fmt.Println("Please enter the name of the new item:")
-	name, _ := in.ReadString('\n')
-	name = strings.TrimSpace(name)
-	menu = append(menu, menuItem{name: name, prices: map[string]float64{}})
-}
-
-func printMenu() {
-	for _, item := range menu {
-		fmt.Println(item.name)
-		fmt.Println(strings.Repeat("-", len(item.name)))
-		for size, price := range item.prices {
-			fmt.Printf("\t%8s%10.2f\n", size, price)
 		}
 	}
 }
--- menu/menu.go
+++ menu/menu.go
@@ -1,1 +1,32 @@
 package menu
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type menuItem struct {
+	name   string
+	prices map[string]float64
+}
+
+var in = bufio.NewReader(os.Stdin)
+
+func AddItem() {
+	fmt.Println("Please enter the name of the new item:")
+	name, _ := in.ReadString('\n')
+	name = strings.TrimSpace(name)
+	menu = append(menu, menuItem{name: name, prices: map[string]float64{}})
+}
+
+func PrintMenu() {
+	for _, item := range menu {
+		fmt.Println(item.name)
+		fmt.Println(strings.Repeat("-", len(item.name)))
+		for size, price := range item.prices {
+			fmt.Printf("\t%8s%10.2f\n", size, price)
+		}
+	}
+}
