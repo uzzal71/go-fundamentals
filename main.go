6a1ac866d49e8925646bcf2f00a7de075df1509a
uzzal71 <uzzalroy.acm@gmail.com>
2023-10-05 23:35:32 +0600

    define menu logic

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 type printer interface {
@@ -17,8 +18,14 @@ func(u user) Print() string {
 	return fmt.Printf("%v [%v]\n", u.username, u.id)
 }
 
-type menuItem(m menuItem) Print() string {
-	
+type menuItem(mi menuItem) Print() string {
+	var b bytes.Buffer 
+	b.WriteString(mi.name + "\n")
+	b.WriteString(strings.Repeat("-", 10) + "\n")
+	for size, cost := range mi.prices {
+		fmt.Fprintf(&b, "\t%10s10.2f\n", size, cost)
+	}
+	return b.String()
 }
 
 func main() {
